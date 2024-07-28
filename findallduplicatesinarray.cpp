cfbaff9c65ecdb59e901dea2581dfac6ebb125ad
thisisamanmishra <amanmishra151202@gmail.com>
2024-07-28 23:42:57 +0530

    duplicates

---
+++ findallduplicatesinarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        map<int, int> count;
+        vector<int> ans;
+        for(int num : nums){
+            if(count.find(num) == count.end()){
+                count[num] = 1;
+            }else{
+                count[num]++;
+                ans.push_back(num);
+            }
+        }
+        return ans;
+    }
+};
